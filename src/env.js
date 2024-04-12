148d722bf1a38f37b3676e2ec15fa3de4fbf5f79
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-12 15:56:42 +0100

    env update

--- src/env.js
+++ src/env.js
@@ -1,3 +1,13 @@
 export default {
-  apikey: 'e62cd896cfb8f1b08039122890ce89a6'
+  apikey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjJjZDg5NmNmYjhmMWIwODAzOTEyMjg5MGNlODlhNiIsInN1YiI6IjYyYzdlYzhkNDQ3ZjljMDA2OTUzYWNkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.om7aqfnnVJwvHBVaHc3Wno9x6D4VP4_d_pkRKtw6c-Y'
+}
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjJjZDg5NmNmYjhmMWIwODAzOTEyMjg5MGNlODlhNiIsInN1YiI6IjYyYzdlYzhkNDQ3ZjljMDA2OTUzYWNkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.om7aqfnnVJwvHBVaHc3Wno9x6D4VP4_d_pkRKtw6c-Y'
+  }
 }
